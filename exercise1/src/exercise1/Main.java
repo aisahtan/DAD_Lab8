03f700de3c6e03d9c7e0ed68da2e43db2c30c923
Aisah Tan <aisahtan99@gmail.com>
2021-05-09 22:09:26 +0800

    initial commit

---
+++ exercise1/src/exercise1/Main.java
@@ -0,0 +1,16 @@
+package exercise1;
+
+public class Main {
+	public static void main (String args[]) {
+		Runnable datePrint = new DateTask();
+		Runnable datePrint1 = new DateTask();
+		
+		// Create objects of thread with name
+		Thread printerThread = new Thread(datePrint, "printerThread [1]");
+		Thread printerThread1 = new Thread(datePrint1, "printerThread [2]");
+		
+		// Execute thread
+		printerThread.start();
+		printerThread1.start();
+	}
+}
